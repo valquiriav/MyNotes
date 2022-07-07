2d1b5c7068d1a445174600c4155e0aef0c2b3e01
Valquíria Oliveira <49369092+valquiriav@users.noreply.github.com>
2022-07-07 19:03:24 -0300

    Set up DI

---
+++ app/src/main/java/com/valquiria/mynotes/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,6 @@
+package com.valquiria.mynotes.feature_note.domain.use_case
+
+data class NoteUseCases (
+    val getNotesUseCase: GetNotesUseCase,
+    val deleteNoteUseCase: DeleteNoteUseCase
+    )
